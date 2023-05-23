701352b033a9c3aa7e1e628b083bcc69c0b9bc79
Stephen Keller <srkeller@uvm.edu>
2023-05-23 14:36:51 -0400

    Update readAccel.R

--- R/readAccel.R
+++ R/readAccel.R
@@ -1,6 +1,6 @@
 #' Read in accelerometer file
 #'
-#' This function reads in an accelerometer file created by Oregon Research Electronics AL100 Acceleration Loggers. 
+#' This function reads in an accelerometer file created by Oregon Research Electronics AL100 or A101 Acceleration Loggers. 
 #' Returns a dataframe with 7 columns: sample number, 3 acceleration axes (x/y/z), temp, seconds, and battery voltage
 #' @param x Location of accelerometer csv file 
 #' @keywords 
@@ -11,7 +11,7 @@
 
 readAccel <- function(x){
   df <- read.table(x, skip=10, sep=",", fill=TRUE, row.names=NULL)
-  df <- na.omit(df)
+  #df <- na.omit(df)
   colnames(df) <- c("sample","x","y","z","temp","sec","Vbatt")
   return(df)
 }
